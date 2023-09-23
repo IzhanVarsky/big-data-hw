61e4c8083c4a7880a5d785cf68c63c9723f8f5f9
IzhanVarsky <48652648+IzhanVarsky@users.noreply.github.com>
2023-09-23 14:21:48 +0300

    hw2 sql statement fixed

--- src/db_utils.py
+++ src/db_utils.py
@@ -4,14 +4,15 @@ import pandas
 
 def write_results(db: gp.Database, dataf: pandas.DataFrame):
     columns = dataf.columns.tolist()
+    cols = ", ".join(columns)
     records = dataf.to_records(index=False).tolist()
 
     # Prepare the SQL statement
-    sql = f"INSERT INTO model_results (%s) VALUES (%s)"
+    sql = f"INSERT INTO model_results ({cols}) VALUES (%s)"
 
     # Execute the SQL statement to insert the data
     cur = db._conn.cursor()
-    cur.executemany(sql, columns, records)
+    cur.executemany(sql, records)
     db._conn.commit()
 
     # Close the cursor and connection
